247899020b44a7d5329680187294519e8553974a
Oriol Deprius <a2207810@institutmontilivi.cat>
2024-09-25 19:57:47 +0200

    canvis

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
 //variables
 let d = new Date();
-let diaMes = d.getDate();
-let mes = d.getMonth() + 1;
+let diaMesActual = d.getDate();
+let mesActual = d.getMonth() + 1;
+let anyActual = d.getFullYear();
+let ultimaDiaMesActual = new Date(anyActual,mesActual + 1,0)
 const diesSetmana = [   //declaro constant perque els dies de la setmana son fixos, els faig servir en un array per 
     "Diumenge",         //convertir el numero entre 0 i 6 que retorna la funcio getDay() en un dia de la setmana.
     "Dilluns",
@@ -10,8 +12,59 @@ const diesSetmana = [   //declaro constant perque els dies de la setmana son fix
     "Dijous",
     "Divendres",
     "Dissabte" ];
-let diaSetmana = diesSetmana[d.getDay()];
+let diaSetmanaActual = diesSetmana[d.getDay()];
 
 //funcio per modificar etiquetes del html a traves del seu id
 function $(e) { return document.getElementById(e) }
 
+//funcio per transformar el numero del mes en lletres
+function nomMes(mesActual) {
+    switch (mesActual) {
+        case 1:
+            return "Gener";
+            break;
+        case 2:
+            return "Febrer";
+            break;
+        case 3:
+            return "Març";
+            break;
+        case 4:
+            return "Abril";
+            break;
+        case 5:
+            return "Maig";
+            break;
+        case 6:
+            return "Juny";
+            break;
+        case 7:
+            return "Juliol";
+            break;
+        case 8:
+            return "Agost";
+            break;
+        case 9:
+            return "Setembre";
+            break;
+        case 10:
+            return "Octubre";
+            break;
+        case 11:
+            return "Novembre";
+            break;
+        case 12:
+            return "Desembre";
+            break;
+    }
+}
+$("nomMes").innerHTML = nomMes(mesActual);
+$("any").innerHTML = anyActual;
+
+function onCauPrimerDiaMes() {
+    
+}
+
+while(diaMesActual <= 31 && ultimaDiaMesActual ){
+
+}

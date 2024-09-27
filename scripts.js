0f78c380f038e26f04f1498c45ce9f55b7ddc98a
Oriol Deprius <a2207810@institutmontilivi.cat>
2024-09-27 17:36:32 +0200

    cositas GORDAS

--- scripts.js
+++ scripts.js
@@ -10,11 +10,61 @@ let numeroMesActual = d.getMonth();
 let anyActual = d.getFullYear();
 let nomMesActual = nomMes(numeroMesActual);
 
-$("mesSelect").value = nomMesActual;
-$("anySelect").value = anyActual;
+selectsActual();
 
-$("nomMes").innerHTML = $("mesSelect").value;
-$("any").innerHTML = $("anySelect").value;
+canviarMesRespecteSelect();
+canviarAnyRespecteSelect();
+pintarCalendari(anyActual, numeroMesActual);
+//quan canvia el valor del select del mes canvia el mes pintat al calendari
+$("mesSelect").onchange = canviarMesRespecteSelect;
+//quan canvia el valor del select de l'any canvia el l'any pintat al calendari
+$("anySelect").onchange = canviarAnyRespecteSelect;
+
+$("btnAvui").onclick = clickAvui;
+
+// $("btnEsquerra").onclick = clickBtnEsquerra;
+
+//posa els selects al mes i any actual
+function selectsActual(){
+    $("mesSelect").value = nomMesActual;
+    $("anySelect").value = anyActual;    
+}
+//canvia el mes escrit al calendari respecte al mes del select
+function canviarMesRespecteSelect(){
+    $("nomMes").innerHTML = $("mesSelect").value;
+}
+//canvia l'any del calendari respecte al mes del select
+function canviarAnyRespecteSelect() {
+    $("any").innerHTML = $("anySelect").value;
+}
+//al fer clic al boto avui canvien els selects i el mes i any pintats al calendari
+function clickAvui() {
+    selectsActual();
+    canviarMesRespecteSelect();
+    canviarAnyRespecteSelect();
+}
+
+// function clickBtnEsquerra(){
+
+// }
+
+function pintarCalendari(any, mes){
+    let d = new Date(any, mes, 1);
+    let dia1Mes = d.getDay();
+    let ultimDiaMes = new Date (any, mes + 1, 0).getDate();
+    let calendari = $("calendari");
+
+    if (dia1Mes == 0){
+        dia1Mes = 7;
+    }
+
+    for(let i = 1; i < dia1Mes; i++){
+        calendari.innerHTML += "<div class='diesCalendari'>&nbsp;</div>";
+    }
+    for(let i = 1; i <= ultimDiaMes; i++ ){
+        calendari.innerHTML += "<div class='diesCalendari'>"+ i +"</div>";
+    }
+}
 
 //funcio per transformar el numero del mes en lletres
 function nomMes(mesActual) {
